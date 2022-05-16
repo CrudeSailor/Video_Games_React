0eef04fccaa25cdef1bc95c61eb921f7c134edaf
Dye <anthony.dye@ies-co.com>
2022-05-16 14:51:13 -0500

    Added Axios to Talk to the BackEnd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import axios from "axios";
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [videoGames, setVideoGames] = useState([]);
+
+useEffect(() => {
+  getVideoGames();
+}, []);
+
+async function getVideoGames() {
+  try{
+    const response = await axios.get("https://localhost:7260/api/games");
+    setVideoGames(response.data);
+  }
+  catch(error){
+    console.log(`ERROR in getVideoGames EXCPETION: ${error}`)
+  }
+}
+
   return (
-    <div>
-      <h3>
-        Hello World
-      </h3>
+    <div className="App">
+    <h1>Video Games!</h1>
+    <p>I'm pretty awesome, just so you know</p>
     </div>
   );
 }
 
 export default App;
+
+
+
